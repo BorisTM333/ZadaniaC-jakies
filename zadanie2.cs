19b134f49b9934e9067d95bf1e7f8ac0f2fe6a92
BorisTM333 <kamila25101997@gmail.com>
2025-11-07 11:40:38 +0100

    zadanie2
    Added basic arithmetic operations and output.

---
+++ zadanie2.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 25;
+        int b = 4;
+
+        Console.WriteLine("Liczba a = " + a);
+        Console.WriteLine("Liczba b = " + b);
+        Console.WriteLine("Suma: " + (a + b));
+        Console.WriteLine("Różnica: " + (a - b));
+        Console.WriteLine("Iloczyn: " + (a * b));
+        Console.WriteLine("Iloraz: " + (a / b));
+        Console.WriteLine("Reszta z dzielenia: " + (a % b));
+    }
+}
